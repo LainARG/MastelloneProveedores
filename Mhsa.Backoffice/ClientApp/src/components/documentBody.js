2458e0e163a2897226f49e5b60bb4c06de721ee6
Diego Cristian Caballero <diego_ns_88@hotmail.com>
2021-05-12 15:56:58 -0300

    commit#11 - test format post

--- Mhsa.Backoffice/ClientApp/src/components/documentBody.js
+++ Mhsa.Backoffice/ClientApp/src/components/documentBody.js
@@ -398,7 +398,6 @@ export default function DocumentBody() {
                 if (allFirstTabData[i][j].numero_documento.includes(e.target.value) == false && e.target.value != "") {
                     suggestions[i].splice(j, 1);
                     setAllFirstTabData(suggestions);
-
                 }
              }
         }
