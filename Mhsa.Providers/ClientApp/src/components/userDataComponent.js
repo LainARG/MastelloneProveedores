0e13842be17066ad46dbad293bd1e21d6088abcb
LainARG <anticristiano03@gmail.com>
2021-09-10 11:53:03 -0300

    commit#87

--- Mhsa.Providers/ClientApp/src/components/userDataComponent.js
+++ Mhsa.Providers/ClientApp/src/components/userDataComponent.js
@@ -24,6 +24,14 @@ return state;
 return "No definido";
 }
 
+
+
+
+
+
+
+
+if(newness != null && newness != undefined && newness != ""){
 return(
 
 <div className="userDataComponentContainer">
@@ -58,9 +66,39 @@ return(
 
 	);
 
-
 }
 
 
+else{
 
+
+return(
+
+<div className="userDataComponentContainer">
+
+<div className="userDataComponentLegend">Sin novedades/avisos para este proveedor.</div><br/>
+<br/>
+<br/>
+<br/>
+
+ <div className="userDataComponentLegend">Tus datos.</div><br/>
+
+		<div className="userDataComponentUserName">Nombre de usuario: &nbsp;{user.props.razon_social}</div>
+		<div className="userDataComponentLocation">Localidad: &nbsp;{user.props.localidad}</div>
+		<div className="userDataComponentCuit">CUIT asociado: &nbsp; {user.props.cuit}</div>
+		<div className="userDataComponentPostalCode">Código Postal: &nbsp; {user.props.codigo_postal}</div>
+		<div className="userDataComponentBusinessName">Razón social: &nbsp; {user.props.razon_social}</div>
+		<div className="userDataComponentPhone">Teléfono: &nbsp; {user.props.telefono} </div>
+		<div className="userDataComponentAddress">Domicilio: &nbsp; {user.props.domicilio}</div>
+ 
+ 
+
+</div>
+	
+
+	);
+
+}
+
+}
 
